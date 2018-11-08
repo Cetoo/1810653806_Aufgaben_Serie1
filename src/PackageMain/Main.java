e7598a2f40924507123e1166e3b9445a13b4d6b7
Cetoo <1810653806@stud.fh-kufstein.ac.at>
2018-11-08 14:10:05 +0100

    dritter commit das Main ein eigenes Package hat

---
+++ src/PackageMain/Main.java
@@ -0,0 +1,9 @@
+public class Main
+{
+
+    public static void main(String[] args) { /*Das ist meine main Methode und somit der Startpunkt meines Programms*/
+        System.out.println("Guten Tag,"); /*Gibt eine Zeile aus mit dem Text Guten Tag*/
+        System.out.println("Dennis Cetin");/*Gibt eine Zeile aus mit dem Text Dennis Cetin*/
+
+    }
+}
